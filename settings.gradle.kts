a3725aefa85c34661a1a0586f51d84168f06a9db
AlexCawl <babushkinsusu@gmail.com>
2023-09-13 20:33:48 +0500

    removed useless deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,10 +12,6 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
-
 rootProject.name = "bookstore-app-backend"
 include(":core")
 include("feature")
